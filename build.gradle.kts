f83056301c3a96d5df444b606833675270bf06c0
raedkebaili <raed.kebaili@gmail.com>
2025-04-26 11:44:26 +0100

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("cloudstream-plugin")
+}
+
+cloudstream {
+    description = "Extension Tuniflix pour Cloudstream"
+    language = "kotlin"
+    authors = listOf("raedkebaili")
+    status = 1
+}
+
+dependencies {
+}
